024343e028dbc2892b9ac22d5a448e892861a618
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-11-06 12:11:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+let a = (document.getElementById("inputA"));
+let b = (document.getElementById("inputB"));
+
+// document.getElementById("addMe").addEventListener("click", () => {
+//     document.getElementById("result").textContent = (Number(a.value) + Number(b.value));
+// });
+
+// document.getElementById("subtractMe").addEventListener("click", () => {
+//     document.getElementById("result").textContent = (a.value - b.value);
+// });
+
+// document.getElementById("divideMe").addEventListener("click", () => {
+//     document.getElementById("result").textContent = (a.value / b.value);
+// });
+
+// document.getElementById("multiplyMe").addEventListener("click", () => {
+//     document.getElementById("result").textContent = (a.value * b.value);
+// });
+
+// document.getElementById("modulusMe").addEventListener("click", () => {
+//     document.getElementById("result").textContent = (a.value % b.value);
+// });
+
+document.getElementById("reset").addEventListener("click", () => {
+    a.value = (""),
+    b.value = (""),
+    document.getElementById("result").textContent = ("");
+});
+
+let buttons = document.querySelectorAll("button");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event){
+        let sum;
+        let btnId = event.target.id;
+
+        switch (btnId) {
+            case "addMe":
+                    sum = Number(a.value) + Number(b.value);
+                    document.getElementById("result").textContent = sum;            
+                    break;
+            case "subtractMe":
+                    sum = a.value - b.value;
+                    document.getElementById("result").textContent = sum;            
+                    break;
+            case "divideMe":
+                    sum = a.value / b.value;
+                    document.getElementById("result").textContent = sum;            
+                    break;
+            case "multiplyMe":
+                    sum = a.value * b.value;
+                    document.getElementById("result").textContent = sum;            
+                    break;
+            case "modulusMe":
+                    sum = a.value % b.value;
+                    document.getElementById("result").textContent = sum;            
+                    break;
+        }
+    })
+}
+
+
